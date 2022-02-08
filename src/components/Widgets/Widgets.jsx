46380291c4d0488ed49913ed9245ee2d427be2dc
Nicolas Magno <66505477+Nicochess@users.noreply.github.com>
2022-02-07 22:45:19 -0300

    style: widget search bar

--- src/components/Widgets/Widgets.jsx
+++ src/components/Widgets/Widgets.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Widgets = () => {
   return (
-    <div>
-      <h2>Widgets</h2>
+    <div className="widgets">
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input type="text" placeholder="Search Twitter" />
+      </div>
+      <div className="widgets__container">
+        <h2>What's happening</h2>
+      </div>
     </div>
   );
 };
